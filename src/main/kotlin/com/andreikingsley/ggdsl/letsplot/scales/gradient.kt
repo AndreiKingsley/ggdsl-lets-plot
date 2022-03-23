a91b71e4da79d3a2e7470c2cbce48657068bb63b
Andrei <kisandy@yandex.ru>
2022-03-23 15:56:54 +0400

    fix v

--- src/main/kotlin/com/andreikingsley/ggdsl/letsplot/scales/gradient.kt
+++ src/main/kotlin/com/andreikingsley/ggdsl/letsplot/scales/gradient.kt
@@ -2,18 +2,17 @@ package com.andreikingsley.ggdsl.letsplot.scales
 
 import com.andreikingsley.ggdsl.dsl.NonPositionalMapping
 import com.andreikingsley.ggdsl.ir.scale.*
-import com.andreikingsley.ggdsl.util.Color
-import jetbrains.letsPlot.scale.scaleColorGradient
+import com.andreikingsley.ggdsl.util.color.*
 import kotlin.reflect.KType
 import kotlin.reflect.typeOf
 
 class ScaleGradient<DomainType>(val domainType: KType) : CustomScale{
     var domainLimits: Pair<DomainType, DomainType>? = null
-    var range: Pair<Color, Color>? = null
+    var range: Pair<StandardColor, StandardColor>? = null
 }
 
 // TODO add BaseContext
-inline infix fun <reified DomainType> NonPositionalMapping<DomainType, Color>.scaleGradient(
+inline infix fun <reified DomainType> NonPositionalMapping<DomainType, StandardColor>.scaleGradient(
     block: ScaleGradient<DomainType>.() -> Unit
 ): ScaleGradient<DomainType>{
     TODO()
