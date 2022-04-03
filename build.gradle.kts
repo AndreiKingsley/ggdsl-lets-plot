79d69eded25d5a7c6d982f4b8e75ba9215de0eed
Andrei <kisandy@yandex.ru>
2022-04-04 01:34:28 +0400

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 val ggdslVersion = "0.2.2-1"
-val ggdslLetsPlotVersion = "0.2.2-4"
+val ggdslLetsPlotVersion = "0.2.2-5"
 
 group = "com.andreikingsley"
 version = ggdslLetsPlotVersion
