c7d6a358def4a251afbc92f40e765516b647f0ed
Andrei <kisandy@yandex.ru>
2022-04-16 14:29:55 +0400

    comment

--- src/main/kotlin/com/andreikingsley/ggdsl/letsplot/scales/guide/LetsPlotAxis.kt
+++ src/main/kotlin/com/andreikingsley/ggdsl/letsplot/scales/guide/LetsPlotAxis.kt
@@ -7,7 +7,7 @@ class LetsPlotAxis<DomainType: Any>: Axis {
     var name: String? = null
     var breaks: List<DomainType>? = null
     var labels: List<String>? = null // todo pair list and format
-
+    // fun overolad
     // breaks(.... format = ) / labeledBreaks(0.0 to "0", 0.4 to ".4" ...)
     // todo expand & trans
 }
