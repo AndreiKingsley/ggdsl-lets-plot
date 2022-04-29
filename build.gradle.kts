1257b86c80bc50a7f401c22da9c86d5577e8912c
Andrei <kisandy@yandex.ru>
2022-04-29 16:22:51 +0400

    update v

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     `maven-publish`
 }
 
-val ggdslVersion = "0.6.1"
-val ggdslLetsPlotVersion = "0.6.1-hotfix-4"
+val ggdslVersion = "0.6.4"
+val ggdslLetsPlotVersion = "0.6.4-1"
 
 group = "com.andreikingsley"
 version = ggdslLetsPlotVersion
